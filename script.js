3cd9c2385b01d7d9d8b8395e560a2a1e175630eb
ennah22 <aboyiblessing2000@gmail.com>
2024-07-31 06:45:05 -0200

    correction

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let buttonchanger= document.getElementById("btn");
 let color = ["blue", "red", "green", "yellow", "pink", "orange", "brown", "Purple", "Lavender", "indigo", "olive"];
-let colorIndex = 0 
-let body = document.querySelector("body")
-let Myspan = document.getElementById("colordisplay")
+let colorIndex = 0;
+let body = document.querySelector("body");
+document.body.style.backgroundColor = "Tomato"
+let Myspan = document.getElementById("colordisplay");
+let MyColorlist = document.getElementById("colorlist");
  
 function button (){
   body.style.backgroundColor = color[colorIndex];
-  colorIndex = (colorIndex + 1 ) % color.length;
-  Myspan.textContent = color[colorIndex]
+  let Colortext = document.createElement("li");
+  Colortext.textContent = color[colorIndex];
+  MyColorlist.appendChild(Colortext);
+  colorIndex = (colorIndex + 1) % color.length;
 }
 
 buttonchanger.addEventListener("click", button); 
-  
